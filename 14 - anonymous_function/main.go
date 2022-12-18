4b1ebc942d7aee266a1bceb61ac0bbbd762e44b0
Célio Júnior <celio_cjr@hotmail.com>
2022-12-18 00:46:46 -0300

    study: change the folder's order

--- 14 - anonymous_function/main.go
+++ 14 - anonymous_function/main.go
@@ -2,24 +2,9 @@ package main
 
 import "fmt"
 
-func sum(numbers ...int) int {
-	total := 0
-
-	for _, number := range numbers {
-		total += number
-	}
-	return total
-}
-
-func writeMessage(text string, years ...int) {
-	for _, year := range years {
-		fmt.Println(text, year)
-	}
-}
-
 func main() {
-	// totalSum := sum(1, 2, 3, 4)
-	// fmt.Println(totalSum)
-
-	writeMessage("oie", 2012, 2013, 2014)
+	returned := func(text string) string {
+		return fmt.Sprintf("received -> %s, %d", text, 25)
+	}("hello world parsed param")
+	fmt.Println(returned)
 }
